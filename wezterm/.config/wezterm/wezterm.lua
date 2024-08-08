d7f0c2ea0e876824ac9965690f3d6a50159a5427
Mark Mekosh <mark.m.mekosh@nasa.gov>
2024-08-08 12:53:36 -0400

    Reorganized wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,29 +1,37 @@
 local wezterm = require('wezterm')
 
 local act = wezterm.action
-local config = {}
+local config = {
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 16,
+  default_cursor_style = 'BlinkingBar',
 
-config.font = wezterm.font('JetBrains Mono')
-config.font_size = 12
--- config.color_scheme = 'Afterglow'
--- config.color_scheme = 'Sonokai (Gogh)'
-config.color_scheme = 'Edge Dark (base16)'
--- config.color_scheme = 'Apprentice (Gogh)'
--- config.color_scheme = 'Andromeda'
-config.default_cursor_style = 'SteadyBar'
+  -- other options: Afterglow, Sonokai (Gogh), Apprentice (Gogh), Andromeda
+  color_scheme = 'Edge Dark (base16)',
 
-config.keys = {
-  -- paste from the clipboard
-  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  -- copy from terminal
-  { key = 'd', mods = 'CTRL', action = act.CopyTo 'ClipboardAndPrimarySelection' },
+  enable_tab_bar = false,
+  -- window_decorations = "RESIZE",
+  window_background_opacity = 0.8,
+  macos_window_background_blur = 10,
+
+  colors = {
+    selection_fg = 'black',
+    selection_bg = '#fffacd',
+  },
+
+  leader = { key = ' ', mods = 'ALT', timeout_milliseconds = 1000 },
+  keys = {
+    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+    { key = 'd', mods = 'CTRL', action = act.CopyTo 'ClipboardAndPrimarySelection' },
+    { key = 't', mods = 'LEADER', action = act({ SpawnCommandInNewTab = { cwd = wezterm.home_dir }})},
+    { key = 'w', mods = 'LEADER', action = act.CloseCurrentPane { confirm=true } },
+    { key = '\\', mods = 'LEADER', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '-', mods = 'LEADER', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
+    { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
+    { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+    { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
+  }
 }
-config.colors = {
-  selection_fg = 'none',
-  selection_bg = 'rgba(0% 0% 0% 0%)',
-}
--- config.window_background_opacity = 0.95
-
--- config.window_background_opacity = 0.95
 
 return config
